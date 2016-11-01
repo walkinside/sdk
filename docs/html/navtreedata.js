bddc4ba1b31b001971a31a25cdb9e2a1c17efaf1
Konstantin Veretennicov <kveretennicov+github@gmail.com>
2016-11-01 21:30:24 +0100

    docs(sdk): add Doxygen-generated content

---
+++ docs/html/navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "COMOS Walkinside APIs", "index.html", [
+    [ "Welcome!", "index.html", null ],
+    [ "API Compatibility Policy", "policy.html", null ],
+    [ "What's New in 10.2", "whats-new.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"index.html":[0],
+"pages.html":[],
+"policy.html":[1],
+"whats-new.html":[2]
+};
