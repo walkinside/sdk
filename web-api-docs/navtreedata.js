306e3ae5d5f28db8509c95b5349009b257cce02a
Konstantin Veretennicov <kveretennicov+github@gmail.com>
2018-05-10 21:51:40 +0200

    docs(web-api): add WalkinsideJS docs for 1.13 release

---
+++ web-api-docs/navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "COMOS Walkinside Server Web APIs", "index.html", [
+    [ "Welcome!", "index.html", null ],
+    [ "WalkinsideJS", "walkinside-js.html", null ],
+    [ "WalkinsideJS Examples", "md_060-walkinside-js-examples.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ web-api-docs/navtreeindex0.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"index.html":[0],
+"md_060-walkinside-js-examples.html":[2],
+"pages.html":[],
+"walkinside-js.html":[1]
+};
