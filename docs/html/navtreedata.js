cffb40f7332fce1ec4ec007c97cf7d1b250429e3
andgonro <andgonro@hotmail.com>
2021-02-15 12:24:05 +0100

    10.3.5 update

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -1,18 +1,17 @@
 /*
-@ @licstart  The following is the entire license notice for the
+@licstart  The following is the entire license notice for the
 JavaScript code in this file.
 
-Copyright (C) 1997-2017 by Dimitri van Heesch
+Copyright (C) 1997-2019 by Dimitri van Heesch
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
@@ -25,8 +24,24 @@ var NAVTREE =
 [
   [ "COMOS Walkinside APIs", "index.html", [
     [ "Welcome!", "index.html", null ],
-    [ "What's New in 10.3", "whats-new.html", null ],
-    [ "API Compatibility Policy", "policy.html", null ]
+    [ "What's New", "md_010-whats-new.html", [
+      [ "10.3.5", "md_010-whats-new.html#autotoc_md1", null ],
+      [ "10.3.4", "md_010-whats-new.html#autotoc_md2", null ],
+      [ "10.3.3", "md_010-whats-new.html#autotoc_md3", null ]
+    ] ],
+    [ "API Compatibility Policy", "policy.html", [
+      [ "API Compatibility", "policy.html#autotoc_md4", null ],
+      [ "API Support Levels", "policy.html#autotoc_md5", null ],
+      [ "Recognized Walkinside APIs", "policy.html#autotoc_md6", null ],
+      [ "\"Empty Shell\" Deprecation", "policy.html#autotoc_md7", null ],
+      [ "Examples", "policy.html#autotoc_md8", [
+        [ "Package Overview", "index.html#autotoc_md12", null ],
+        [ "Contact Us", "index.html#autotoc_md13", null ],
+        [ "Example 1.", "policy.html#autotoc_md9", null ],
+        [ "Example 2.", "policy.html#autotoc_md10", null ]
+      ] ],
+      [ "API Versioning", "policy.html#autotoc_md11", null ]
+    ] ]
   ] ]
 ];
 
